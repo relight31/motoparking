57e1b26989183c4e2fa40b24699a6dd1dcba0f0c
relight31 <halfbcsina@yahoo.com.sg>
2021-10-21 14:15:33 +0800

    Auto-populate HDB CP locations via data.gov API call (1st 100 CPs only)

--- src/Map.js
+++ src/Map.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
-import { MapContainer, TileLayer, Marker, Popup, Tooltip } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  CircleMarker,
+  Popup,
+  Tooltip,
+} from "react-leaflet";
 import { OpenStreetMapProvider } from "react-leaflet-geosearch";
 import SearchControl from "./SearchControl";
+import { GetData } from "./datafromAPI.js";
 import LocationList from "./locations.json";
 
 export default function Map(props) {
-
   let [filteredLocations, setFilteredLocation] = useState(LocationList);
 
   const Carpark = (props) => {
@@ -21,6 +28,23 @@ export default function Map(props) {
     );
   };
 
+  const HDBcarpark = (props) => {
+    return (
+      <CircleMarker
+        center={props.coords}
+        pathOptions={{ color: "red" }}
+        radius={10}
+      >
+        <Tooltip sticky>
+          HDB Carpark <br /> {props.name}
+          <br /> Free with CSPT
+        </Tooltip>
+      </CircleMarker>
+    );
+  };
+
+  let hdbdataset = GetData();
+  console.log(hdbdataset);
   const prov = OpenStreetMapProvider();
 
   return (
@@ -33,7 +57,9 @@ export default function Map(props) {
         {filteredLocations.map((location, index) => {
           return <Carpark key={index} {...location} />;
         })}
-
+        {hdbdataset.map((location, index) => {
+          return <HDBcarpark key={index} {...location} />;
+        })}
         <SearchControl
           provider={prov}
           showMarker={true}
